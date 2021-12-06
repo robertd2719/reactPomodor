0ed9aca09a9697c366520973ba108191b57fad1b
robertd2719 <45602920+robertd2719@users.noreply.github.com>
2021-12-05 21:21:01 -0500

    back end complete with full restful CRUD API restful server and database connection ..updated Timer.js to reflect state of mins and seconds ...now displaying properly

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,30 @@
+import {useState} from "react";
+
 const Timer = () =>{
+    const [minutes, updateMinutes] = useState(0);
+    const [seconds, updateSeconds] = useState(0);
+
+    let minutesDisplay = (minutes<9) ? "0"+minutes.toString() : minutes.toString();
+    let secondsDisplay = (minutes<9) ? "0"+seconds.toString() : seconds.toString();
+
+    const incrementDecrementMinutes = value =>{
+        updateMinutes(prevValue =>prevValue + value);
+    }
+
+    const incrementDecrementSeconds = value =>{
+        updateSeconds(prevValue =>prevValue + value);
+    }
+
     return (
-        <h1>Timer component has been initialized</h1>
+        <>
+            <h2>TIMER: {minutesDisplay}: {secondsDisplay}</h2>
+            <h3>Minutes Buttons</h3>
+            <button onClick={()=>incrementDecrementMinutes(1)}>increment</button>
+            <button onClick={()=>incrementDecrementMinutes(-1)}>decrement</button>
+            <h3>Seconds Buttons</h3>
+            <button onClick={()=>incrementDecrementSeconds(1)}>increment</button>
+            <button onClick={()=>incrementDecrementSeconds(-1)}>decrement</button>
+        </>
     );
 }
 
